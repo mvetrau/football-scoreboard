8e1720d14dfd6e73a260c899582b8fd411e37489
Mikhail Vetrau <mvetrau@gmail.com>
2025-03-20 18:08:53 +0100

    test: Add failing tests for starting match

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -6,4 +6,24 @@ describe('Football Scoreboard', () => {
   beforeEach(() => {
     scoreboard = new Scoreboard();
   });
+
+  test('should start a new match with a score of 0-0', () => {
+    scoreboard.startMatch('Mexico', 'Canada');
+    expect(scoreboard.getSummary()).toEqual([
+      { home: 'Mexico', away: 'Canada', homeScore: 0, awayScore: 0 },
+    ]);
+  });
+
+  test('should not allow a match where home and away teams are the same', () => {
+    expect(() => scoreboard.startMatch('Brazil', 'Brazil')).toThrow(
+      'Home and Away teams must be different.'
+    );
+  });
+
+  test('should not allow duplicate matches in progress', () => {
+    scoreboard.startMatch('Brazil', 'Argentina');
+    expect(() => scoreboard.startMatch('Brazil', 'Argentina')).toThrow(
+      'This match is already in progress.'
+    );
+  });
 });
