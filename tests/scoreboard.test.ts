843d646bc6c74d0b7bdff13419f952fce7682003
Mikhail Vetrau <mvetrau@gmail.com>
2025-03-20 18:57:25 +0100

    test: Add failing tests for getting summary

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -60,4 +60,42 @@ describe('Football Scoreboard', () => {
   test('should not allow finishing a non-existing match', () => {
     expect(() => scoreboard.finishMatch('England', 'Italy')).toThrow('Match not found.');
   });
+
+  test('should order matches by total score', () => {
+    scoreboard.startMatch('Spain', 'Brazil');
+    scoreboard.updateScore('Spain', 'Brazil', 3, 3);
+
+    scoreboard.startMatch('Mexico', 'Canada');
+    scoreboard.updateScore('Mexico', 'Canada', 2, 1);
+
+    const expected: Match[] = [
+      { home: 'Spain', away: 'Brazil', homeScore: 3, awayScore: 3 },
+      { home: 'Mexico', away: 'Canada', homeScore: 2, awayScore: 1 },
+    ];
+
+    expect(scoreboard.getSummary()).toEqual(expected);
+  });
+
+  test('should order matches by recency if scores are equal', () => {
+    scoreboard.startMatch('Poland', 'Albania');
+    scoreboard.updateScore('Poland', 'Albania', 5, 2);
+
+    scoreboard.startMatch('Mexico', 'Canada');
+    scoreboard.updateScore('Mexico', 'Canada', 2, 2);
+
+    scoreboard.startMatch('Spain', 'Brazil');
+    scoreboard.updateScore('Spain', 'Brazil', 1, 3);
+
+    scoreboard.startMatch('Germany', 'France');
+    scoreboard.updateScore('Germany', 'France', 2, 2);
+
+    const expected: Match[] = [
+      { home: 'Poland', away: 'Albania', homeScore: 5, awayScore: 2 },
+      { home: 'Germany', away: 'France', homeScore: 2, awayScore: 2 },
+      { home: 'Spain', away: 'Brazil', homeScore: 1, awayScore: 3 },
+      { home: 'Mexico', away: 'Canada', homeScore: 2, awayScore: 2 },
+    ];
+
+    expect(scoreboard.getSummary()).toEqual(expected);
+  });
 });
